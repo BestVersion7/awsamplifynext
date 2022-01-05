b31c966eda713f6ceb0b7b63af561b6cb792469a
BestVersion7 <hunterkfan@gmail.com>
2022-01-04 23:18:25 -0500

    converted to prsima

--- utils/apiCall.js
+++ utils/apiCall.js
@@ -4,15 +4,27 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    host: process.env.PG_HOST,
+    user: "postgres",
+    password: "password",
+    database: "sample",
+    host: "localhost",
     port: 5432,
-    ssl: {
-        rejectUnauthorized: false,
-    },
 });
+
+import prisma from "../lib/prisma";
+
+//
+
+// const pool = new Pool({
+//     user: process.env.PG_USER,
+//     password: process.env.PG_PASSWORD,
+//     database: process.env.PG_DATABASE,
+//     host: process.env.PG_HOST,
+//     port: 5432,
+//     ssl: {
+//         rejectUnauthorized: false,
+//     },
+// });
 
 const selectAll = () => pool.query(`SELECT * FROM usertb ORDER BY account_id;`);
 const selectTen = () =>
@@ -20,10 +32,21 @@ const selectTen = () =>
 const fetchByName = (name) =>
     pool.query(`SELECT * FROM usertb WHERE first_name = $1`, [name]);
 
+// export const fetchAll = async () => {
+//     try {
+//         const data1 = await prisma.usertb.findMany()
+//         const data = await selectAll();
+//         return data.rows;
+//     } catch (err) {
+//         return {
+//             notFound: true,
+//         };
+//     }
+// };
 export const fetchAll = async () => {
     try {
-        const data = await selectAll();
-        return data.rows;
+        const data = await prisma.usertb.findMany();
+        return data;
     } catch (err) {
         return {
             notFound: true,
@@ -32,8 +55,8 @@ export const fetchAll = async () => {
 };
 export const fetchTen = async () => {
     try {
-        const data = await selectTen();
-        return data.rows;
+        const data = await prisma.usertb.findMany({ take: 10 });
+        return data;
     } catch (err) {
         return {
             notFound: true,
@@ -43,8 +66,16 @@ export const fetchTen = async () => {
 
 export const fetchName = async (name) => {
     try {
-        const data = await fetchByName(name);
-        return data.rows[0];
+        const data = await prisma.usertb.findFirst({
+            where: { first_name: name },
+            select: {
+                first_name: true,
+                last_name: true,
+                gender: true
+            }
+        });
+        // console.log(data)
+        return data;
     } catch (err) {
         return undefined;
     }
