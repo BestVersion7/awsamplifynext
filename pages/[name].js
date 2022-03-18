acb465c537f4abd5bdd3c62e53a19212a54bb426
BestVersion7 <hunterkfan@gmail.com>
2022-03-17 22:21:14 -0400

    initial

--- pages/[name].js
+++ pages/[name].js
@@ -1,48 +1,54 @@
-import { fetchTen, fetchName } from "../utils/apiCall";
-import Link from "next/link";
-import { useRouter } from "next/router";
+// import { fetchTen, fetchName } from "../utils/apiCall";
+// import Link from "next/link";
+// import { useRouter } from "next/router";
 
-const NamePage = ({ data }) => {
-    const router = useRouter();
+// const NamePage = ({ data }) => {
+//     const router = useRouter();
 
-    if (router.isFallback) {
-        return <div>Loading</div>;
-    }
-    return (
-        <section>
-            Name is {data.first_name} <br /> Gender is {data.gender}
-            <br /> Last name is {data.last_name} <br />{" "}
-            <Link href="/">Home</Link>
-        </section>
-    );
+//     if (router.isFallback) {
+//         return <div>Loading</div>;
+//     }
+//     return (
+//         <section>
+//             Name is {data.first_name} <br /> Gender is {data.gender}
+//             <br /> Last name is {data.last_name} <br />{" "}
+//             <Link href="/">Home</Link>
+//         </section>
+//     );
+// };
+
+// export async function getStaticProps({ params }) {
+//     const data = await fetchName(params.name);
+//     if (!data) {
+//         return {
+//             notFound: true,
+//         };
+//     }
+//     return {
+//         props: {
+//             data,
+//         },
+//         revalidate: 3600,
+//     };
+// }
+
+// export async function getStaticPaths() {
+//     const data = await fetchTen();
+//     const paths = data.map((item) => ({
+//         params: {
+//             name: item.first_name,
+//         },
+//     }));
+//     return {
+//         paths,
+//         fallback: true,
+//     };
+// }
+
+// export default NamePage;
+
+const NamePage = () => {
+    return <div>hello</div>;
 };
-
-export async function getStaticProps({ params }) {
-    const data = await fetchName(params.name);
-    if (!data) {
-        return {
-            notFound: true,
-        };
-    }
-    return {
-        props: {
-            data,
-        },
-        revalidate: 3600,
-    };
-}
-
-export async function getStaticPaths() {
-    const data = await fetchTen();
-    const paths = data.map((item) => ({
-        params: {
-            name: item.first_name,
-        },
-    }));
-    return {
-        paths,
-        fallback: true,
-    };
-}
 
 export default NamePage;
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,11 @@
-import { fetchAll } from "../utils/apiCall";
-import Link from "next/link";
+// import { fetchAll } from "../utils/apiCall";
+// import Link from "next/link";
 
-export default function Home({ data }) {
+export default function Home() {
     return (
         <div>
-            {data.map((item) => (
-                <div key={item.account_id}>
-                    <h2>
-                        Name is {item.first_name}; Gender is {item.gender}{" "}
-                        <Link href={`/${item.first_name}`}>
-                            {item.first_name}
-                        </Link>
-                    </h2>
-                </div>
-            ))}
+            hello
         </div>
     );
 }
 
-export async function getStaticProps() {
-    const data = await fetchAll();
-
-    return {
-        props: {
-            data,
-        },
-    };
-}
